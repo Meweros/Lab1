c1acad806b939c5cf048113217d6e4da329a6935
A.L.Kosmakov <a.l.kosmakov@urfu.ru>
2024-02-15 17:32:57 +0500

    Initial commit

---
+++ Lab1M2.h
@@ -0,0 +1,14 @@
+//
+// Created by apbob on 15.02.2024.
+//
+
+#ifndef LAB1_LAB1M2_H
+#define LAB1_LAB1M2_H
+
+
+class Lab1M2 {
+
+};
+
+
+#endif //LAB1_LAB1M2_H
---
+++ Lab1M3.h
@@ -0,0 +1,14 @@
+//
+// Created by apbob on 15.02.2024.
+//
+
+#ifndef LAB1_LAB1M3_H
+#define LAB1_LAB1M3_H
+
+
+class Lab1M3 {
+
+};
+
+
+#endif //LAB1_LAB1M3_H
